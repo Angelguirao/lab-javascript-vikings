32bfc45c73aaf23d41558799e84d83988b974a48
Angel Guirao <angeguirao92@gmail.com>
2023-06-16 16:50:22 +0200

    solved lab with bonus and a sample game!

--- src/viking.js
+++ src/viking.js
@@ -54,14 +54,82 @@ class War {
     constructor() {
         this.vikingArmy = [];
         this.saxonArmy = [];
+    } 
+      addViking(viking) {
+        this.vikingArmy.push(viking);
+      }
+
+      addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+      }
+    
+      vikingAttack() {
+        const randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+        const randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+        const randomSaxon = this.saxonArmy[randomSaxonIndex];
+        const randomViking = this.vikingArmy[randomVikingIndex];
+    
+        const result = randomSaxon.receiveDamage(randomViking.strength);
+    
+        if (randomSaxon.health <= 0) {
+          this.saxonArmy.splice(randomSaxonIndex, 1);
+        }
+    
+        return result;
+      }
+    
+      saxonAttack() {
+        const randomSaxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+        const randomVikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+        const randomSaxon = this.saxonArmy[randomSaxonIndex];
+        const randomViking = this.vikingArmy[randomVikingIndex];
+    
+        const result = randomViking.receiveDamage(randomSaxon.strength);
+    
+        if (randomViking.health <= 0) {
+          this.vikingArmy.splice(randomVikingIndex, 1);
+        }
+    
+        return result;
+      }
+    
+      showStatus() {
+        if (this.vikingArmy.length === 0) {
+          return "Saxons have fought for their lives and survived another day...";
+        } else if (this.saxonArmy.length === 0) {
+          return "Vikings have won the war of the century!";
+        } else {
+          return "Vikings and Saxons are still in the thick of battle.";
+        }
+      }
     }
 
-    addViking(vikingInstance) {
-        
-    }
-    addSaxon()
-    vikingAttack()
-    saxonAttack()
-    showStatus()
+    // Let's play the game!!!
+
+    const WorldwarIII = new War();
+
+    const viking1 = new Viking("Jaime", 100, 80);
+    const viking2 = new Viking("Brian", 100, 60);
+    const viking3 = new Viking("Angel", 100, 40);
+
+    WorldwarIII.addViking(viking1);
+    WorldwarIII.addViking(viking2);
+    WorldwarIII.addViking(viking3);
+
+    const saxon1 = new Saxon(100, 100);
+    const saxon2 = new Saxon(100, 35);
+    const saxon3 = new Saxon(100, 35);
+
+    WorldwarIII.addSaxon(saxon1);
+    WorldwarIII.addSaxon(saxon2);
+    WorldwarIII.addSaxon(saxon3);
+
+    while (!(WorldwarIII.vikingArmy.length === 0) || !(WorldwarIII.saxonArmy.length === 0)) {
+    const vikingResult = WorldwarIII.vikingAttack();
+    const saxonResult = WorldwarIII.saxonAttack();
+    const battleResult = WorldwarIII.showStatus();
+
+    console.log(`Viking attack result: ${vikingResult}`);
+    console.log(`Saxon attack result: ${saxonResult}`);
+    console.log(battleResult);
 }
-
